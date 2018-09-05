e52677a816fa565e5e4d0e98c9b41d59c8a2a6ed
sanekrash <sanekrash@yandex.ru>
2018-09-05 18:24:39 +0300

    Add spellcard

--- NupskouProject/Rashka/Bullets/VerticalBounceRoundBullet.cs
+++ NupskouProject/Rashka/Bullets/VerticalBounceRoundBullet.cs
@@ -30,7 +30,7 @@ namespace NupskouProject.Rashka.Bullets {
 
         protected override void Update (int t) {
             _p   = _p0 + t * _v;
-            _p.X = Mathf.PingPong (_p.X - 30, World.Box.Right) + 30;
+            _p.X = Mathf.PingPong (_p.X, World.Box.Right - 30) + 30;
             if (_p.Y > World.Box.Bottom + 6) {
                 Despawn ();
             }
