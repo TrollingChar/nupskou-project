f907a997324408d8a9fa0eb0ebcc30882940ec6c
trollingchar <rodind-1@mail.ru>
2018-09-06 14:04:40 +0300

    round bullet fix

--- NupskouProject/Entities/RoundBullet.cs
+++ NupskouProject/Entities/RoundBullet.cs
@@ -9,9 +9,6 @@ namespace NupskouProject.Entities {
 
     public abstract class RoundBullet : StdEntity {
 
-//        private readonly XY _p0;
-//        private readonly XY _v;
-
         protected XY    P;
         protected float R;
         protected Color MainColor, BorderColor;
@@ -20,28 +17,13 @@ namespace NupskouProject.Entities {
         public override Hitbox PlayerDamagerHitbox => new CircleHitbox (P, R);
 
 
-//        public RoundBullet (XY p0, XY v) {
-//            P = _p0 = p0;
-//            _v = v;
-//            R = 5;
-//        }
-
-
         public override void OnStrike (Entity entity) => Despawn ();
-
-
-        protected override void Update (int t) {
-//            P = _p0 + t * _v;
-//            if (t > 30 && !Geom.CircleOverBox (new Circle(P, R + 2), World.Box)) {
-//                Despawn ();
-//            }
-        }
 
 
         public override void Render () {
             _.Renderer.TestForeground.Add (
-                new Sprite (_.Assets.Circle) {Position = P, Color = MainColor, Scale = new Vector2 (R + 1)},
-                new Sprite (_.Assets.Circle) {Position = P, Color = BorderColor,  Scale = new Vector2 (R + 0.5f)},
+                new Sprite (_.Assets.Circle) {Position = P, Color = BorderColor, Scale = new Vector2 (R + 1)},
+                new Sprite (_.Assets.Circle) {Position = P, Color = MainColor,   Scale = new Vector2 (R + 0.5f)},
                 new Sprite (_.Assets.Circle) {Position = P, Color = Color.White, Scale = new Vector2 (R)}
             );
         }
