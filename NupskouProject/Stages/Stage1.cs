604611139431e27289fc68c6f68c5a1672c6d5cf
sanekrash <sanekrash@yandex.ru>
2018-11-04 13:00:45 +0300

    some changes

--- NupskouProject/Stages/Stage1.cs
+++ NupskouProject/Stages/Stage1.cs
@@ -10,26 +10,26 @@ namespace NupskouProject.Stages {
     public class Stage1 : StdEntity {
         protected override void Update (int t) {
             switch (t) {
-                case 120:
+                case 60:
                     _.World.Spawn(new Clock(
                             i => _.World.Spawn(new EnemyMastirStage1Encounter1(
                                 new XY(World.Box.Left, World.Box.Bottom * 0.1f), XY.Right, i % _.Difficulty.Choose (6, 3, 2, 2)  == 0, -1)),
                             12,
-                            //Раден, ты ⑨
-                            30,
-                            0
-                        )
-                    );
-                    _.World.Spawn(new Clock(
-                            i => _.World.Spawn(new EnemyMastirStage1Encounter1(
-                                new XY(World.Box.Right, World.Box.Bottom * 0.1f),   XY.Left, i % _.Difficulty.Choose (6, 3, 2, 2)  == 0, 1)),
-                            12,
+                            //Раден,не забудь уклониться от всех пуль ты ⑨
                             30,
                             0
                         )
                     );
                     break;
-                case 600:
+                case 120:
+                    _.World.Spawn(new Clock(
+                            i => _.World.Spawn(new EnemyMastirStage1Encounter1(
+                                new XY(World.Box.Right, World.Box.Bottom * 0.15f),   XY.Left, i % _.Difficulty.Choose (6, 3, 2, 2)  == 0, 1)),
+                            12,
+                            30,
+                            0
+                        )
+                    );
                     break;
                 case 900:
                     break;
