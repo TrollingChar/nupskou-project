746ea54fe10875daa482754ecb7392d60f25a7bd
trollingchar <rodind-1@mail.ru>
2019-02-22 21:46:21 +0300

    упростил спеллкарту

--- NupskouProject/Raden/TestSpellcard/TestSpellcard.cs
+++ NupskouProject/Raden/TestSpellcard/TestSpellcard.cs
@@ -9,12 +9,36 @@ namespace NupskouProject.Raden.TestSpellcard {
     public class TestSpellcard : StdEntity {
 
         protected override void Update (int t) {
-            var v = new XY (t * Mathf.phiAngle / 2);
-            foreach (var w in Danmaku.Ring (v, 2)) {
-                _.World.Spawn (new LinearRoundBullet       (World.BossPlace,    3 * w, Color.Green,  Color.Lime));
-                _.World.Spawn (new SqrtRoundBullet         (World.BossPlace,   30 * w, Color.Blue,   Color.LightBlue));
-                _.World.Spawn (new AcceleratingRoundBullet (World.BossPlace, 0.3f * w, Color.Maroon, Color.Red));
-            }
+            var v = new XY (t * Mathf.phiAngle);
+//            foreach (var w in Danmaku.Ring (v, 2)) {
+            _.World.Spawn (
+                new LinearRoundBullet (
+                    World.BossPlace,
+                    3 * v,
+                    Color.Green,
+                    Color.Lime,
+                    8
+                )
+            );
+            _.World.Spawn (
+                new SqrtRoundBullet (
+                    World.BossPlace,
+                    30 * v,
+                    Color.Blue,
+                    Color.LightBlue,
+                    8
+                )
+            );
+            _.World.Spawn (
+                new AcceleratingRoundBullet (
+                    World.BossPlace,
+                    0.3f * v,
+                    Color.Maroon,
+                    Color.Red,
+                    8
+                )
+            );
+//            }
         }
 
     }
