3a4adf4610ef10339cc01dac4075fb674c9d043a
trollingchar <rodind-1@mail.ru>
2019-02-23 12:29:23 +0300

    revolt 2 difficulty

--- NupskouProject/Raden/Revolt2/Revolt2.cs
+++ NupskouProject/Raden/Revolt2/Revolt2.cs
@@ -3,6 +3,7 @@ using NupskouProject.Core;
 using NupskouProject.Entities;
 using NupskouProject.Math;
 using NupskouProject.Raden.Bullets;
+using NupskouProject.Util;
 
 
 namespace NupskouProject.Raden.Revolt2 {
@@ -10,16 +11,16 @@ namespace NupskouProject.Raden.Revolt2 {
     public class Revolt2 : StdEntity {
 
         protected override void Update (int t) {
-            // ИЗИ МОДО
             var   o  = World.Box.Center;
-            float av = Mathf.PI / 1200;
+            float av = Mathf.PI / _.Difficulty.Choose (1200, 800, 1200, 800);
+            int dt = _.Difficulty.Choose (90, 90, 60, 60);
             if (t == 0) {
                 _.World.Spawn (new HugeStar (new XY (o.X, World.Box.Top    + 20), XY.Zero, 0, av));
                 _.World.Spawn (new HugeStar (new XY (o.X, World.Box.Bottom - 20), XY.Zero, 0, av));
             }
-            if (t % 120 == 0) {
+            if (t % dt == 0) {
                 // ReSharper disable once PossibleLossOfFraction - так и было задумано!
-                float angle = t / 60 * Mathf.phiAngle / 30;
+                float angle = t / dt * Mathf.phiAngle / 30;
                 foreach (var v in Danmaku.Ring (new XY (angle), 30)) {
                     _.World.Spawn (new LinearRoundBullet (new XY(World.Box.Left,  o.Y), v, Color.Red, Color.Red));
                     _.World.Spawn (new LinearRoundBullet (new XY(World.Box.Right, o.Y), v, Color.Red, Color.Red));
