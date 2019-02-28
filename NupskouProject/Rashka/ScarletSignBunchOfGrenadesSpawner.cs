091804fbc7c4edb1ebfd432bbceaed087456414f
trollingchar <rodind-1@mail.ru>
2019-02-28 23:45:17 +0300

    бля санек ну ты и ⑨ конечно

--- NupskouProject/Rashka/ScarletSignBunchOfGrenadesSpawner.cs
+++ NupskouProject/Rashka/ScarletSignBunchOfGrenadesSpawner.cs
@@ -5,77 +5,62 @@ using NupskouProject.Math;
 using NupskouProject.Rashka.Bullets;
 using NupskouProject.Util;
 
-namespace NupskouProject.Rashka
-{
-    public class ScarletSignBunchOfGrenadesSpawner : StdEntity
-    {
-        private int N = _.Difficulty.Choose(18,30,30,42);
-        private XY[] coordinate = new XY[_.Difficulty.Choose(18,30,30,42)];
-        private XY _p;
+
+namespace NupskouProject.Rashka {
+
+    public class ScarletSignBunchOfGrenadesSpawner : StdEntity {
+
+        private int   N          = _.Difficulty.Choose (18, 30, 30, 42);
+        private XY [] coordinate = new XY[_.Difficulty.Choose (18, 30, 30, 42)];
+        private XY    _p;
 
 
-        public ScarletSignBunchOfGrenadesSpawner(XY p)
-        {
+        public ScarletSignBunchOfGrenadesSpawner (XY p) {
             _p = p;
         }
 
 
-
-        protected override void Update(int t)
-        {
+        protected override void Update (int t) {
             var world = _.World;
-            if (t % 360 == 0)
-            {
-                for (int i = 0; i < N; i++)
-                {
-                    coordinate[i] = new XY(-9000, -9000);
-                }
+            
+            if (t % 360 != 0) return;
 
-                for (int i = 0; i < N; i++)
-                {
-                    bool g = false;
-                    int attempts = 0;
-                    while (g == false && attempts < 10)
-                    {
-                        var a = _.Random.Point(World.Box);
-                        g = Check(a, i);
-                        attempts++;
-                    }
+            for (int i = 0; i < N; i++) {
+                coordinate [i] = new XY (-9000, -9000);
+            }
 
-                }
+            for (int i = 0; i < N; i++)
+            for (int attempt = 0; attempt < 10; attempt++) {
+                var a = _.Random.Point (World.Box);
+                if (Check (a, i)) break;
+            }
 
-                for (int j = 0; j < N; j++)
-                {
-                    world.Spawn(new GrenadeBullet(
+            for (int j = 0; j < N; j++) {
+                world.Spawn (
+                    new GrenadeBullet (
                         _p,
-                        new XY(XY.DirectionAngle(_p, coordinate[j])).WithLength(XY.Distance(_p, coordinate[j]) / 150),
+                        (coordinate [j] - _p) / 150,
                         Color.Red,
                         Color.Red,
                         5f,
-                        150)
-                    );
-                }
+                        150
+                    )
+                );
             }
-
-
 
         }
 
-        private bool Check(XY a, int index)
-        {
-            for (int i = 0; i < N; i++)
-            {
-                if (XY.Distance(a, coordinate[i]) < 10)
-                {
-                    return false;
-                }
-            }
-            if (a.Y < World.PlayerBox.Bottom*0.95)
 
-            coordinate[index] = a;
+        private bool Check (XY a, int index) {
+            for (int i = 0; i < N; i++) {
+                if (XY.SqrDistance (a, coordinate [i]) < 100) return false;
+            }
+            if (a.Y < World.PlayerBox.Bottom * 0.95) {
+                coordinate [index] = a;
+            }
             return true;
-
         }
-    }
-}
 
+    }
+
+}
