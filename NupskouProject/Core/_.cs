952f32e152ceaa0028e2ad01c456e9d09d627fae
trollingchar <rodind-1@mail.ru>
2019-03-01 19:56:35 +0300

    ...

--- NupskouProject/Core/_.cs
+++ NupskouProject/Core/_.cs
@@ -15,13 +15,13 @@ namespace NupskouProject.Core {
         public static int             Score           = 0;
         public static int             ScoreValue      = 10000;
         public static int             Graze           = 0;
-        public static int             Lives       = 3;
-        public static int             Bombs       = 3;
+        public static int             Lives           = 3;
+        public static int             Bombs           = 3;
         
         public static Renderer        Renderer        = new Renderer ();
         public static Assets          Assets          = new Assets ();
         public static Random          Random          = new Random ();
-        public static Difficulty      Difficulty      = Difficulty.Lunatic;
+        public static Difficulty      Difficulty      = Difficulty.Normal;
         public static PlayerCharacter PlayerCharacter = PlayerCharacter.Rashka;
         public static Player          Player;
 
